5cfa44dd305d4ffb736bfd809ed12c826c8e6871
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-08-22 18:40:38 +0200

    this version is working up to the stuff generation

--- fashionsite/chardata/fashion_action.py
+++ fashionsite/chardata/fashion_action.py
@@ -127,7 +127,7 @@ def fashion(request, char_id, spells=False):
     if spells:
         return HttpResponseRedirect(reverse('chardata.spells_view.spells', args=(char.id,)))
     
-    return HttpResponseRedirect(reverse('solution', args=(char.id,)))
+    return HttpResponseRedirect(reverse('solution_2', args=(char.id,)))
 
 def set_stats(char, stats):
     for element_name, abr in STATS_NAMES:
