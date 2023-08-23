6180e6956a62893656d10c267ca81be73d105410
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-08-23 20:57:09 +0200

    Update wsgi.py

--- fashionsite/fashionsite/wsgi.py
+++ fashionsite/fashionsite/wsgi.py
@@ -24,6 +24,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/var/www/DofusFashionistaVanced/fashionsite')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashionsite.settings")
 
 from django.core.wsgi import get_wsgi_application
