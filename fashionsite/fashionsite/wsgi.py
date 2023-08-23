8a23c26cf24448b03522ce57568401acaa25f004
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-08-23 21:07:55 +0200

    Update wsgi.py

--- fashionsite/fashionsite/wsgi.py
+++ fashionsite/fashionsite/wsgi.py
@@ -26,8 +26,18 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append('/var/www/DofusFashionistaVanced/fashionsite/fashionsite/fashionsite')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashionsite.settings")
+with open('/etc/fashionista/config') as f:
+    path = f.read().strip()  # using strip() to remove any leading/trailing whitespace
+
+sys.path.append(path)  # Adding the main project directory to sys.path
+sys.path.append('/home/ec2-user/DofusFashionistaVanced')
+sys.path.append('/home/ec2-user/DofusFashionistaVanced/fashionistapulp')
+sys.path.append('/home/ec2-user/.local/lib/python3.9/site-packages')
+sys.path.append('/usr/local/lib/python3.9/site-packages')
+sys.path.append(os.path.join(path, 'fashionistapulp'))
+sys.path.append(os.path.join(path, 'fashionsite'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fashionsite.settings' 
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
