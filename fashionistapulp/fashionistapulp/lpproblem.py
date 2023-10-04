2b077b0b4c8091a8e7bde616f44f35bde2510d76
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-10-04 19:11:07 +0200

    add char_id to save project

--- fashionistapulp/fashionistapulp/lpproblem.py
+++ fashionistapulp/fashionistapulp/lpproblem.py
@@ -53,7 +53,6 @@ class LpProblem2:
 
     def setup_variable(self, category, id, min_bound, max_bound):
         name = '%s_%s' % (category, str(id))
-        print(f"Setting up variable: {name}")  # Debugging line
         pulpVar = LpVariable(name, min_bound, max_bound, LpInteger)
         self.pulp_vars[name] = pulpVar
 
@@ -62,7 +61,6 @@ class LpProblem2:
 
     def add_to_of(self, category, id, weight):
         var_name = '%s_%s' % (category, str(id))
-        print(f"Adding to objective function: {var_name}")  # Debugging line
         if self.obj_vars.get(var_name) == None:
             self.obj_vars[var_name] = weight
         else:
