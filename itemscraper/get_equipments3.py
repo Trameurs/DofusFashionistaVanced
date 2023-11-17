a48020b649a943dc98f8652709fde39e2f61b1d8
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-11-17 19:26:10 +0100

    fixed ","

--- itemscraper/get_equipments3.py
+++ itemscraper/get_equipments3.py
@@ -272,7 +272,7 @@ with open('../fashionistapulp/fashionistapulp/item_db_dumped.dump', 'w', encodin
                     if element == 'neutral':
                         element = 'neut'
 
-                    f.write(f"INSERT INTO weapon_hits VALUES({index},{item['uses_per_turn']},{min_value},{max_value},{steals},{heals}'{element}');\n")
+                    f.write(f"INSERT INTO weapon_hits VALUES({index},{item['uses_per_turn']},{min_value},{max_value},{steals},{heals},'{element}');\n")
 
     f.write("""CREATE TABLE extra_lines (item INTEGER, line text, language text, FOREIGN KEY(item) REFERENCES items(id));\n""")
 
