28605b532b9736bc0ec12787e5a7973d0062733b
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-08 19:00:48 +0100

    Update settings.py

--- fashionsite/fashionsite/settings.py
+++ fashionsite/fashionsite/settings.py
@@ -186,11 +186,11 @@ USE_TZ = True
 STATIC_FILES_BUCKET = 'fashionistavanced'
 with open(os.path.join(CONFIG_DIR, 'serve_static')) as f:
     serve_static = True #f.read().startswith('True')
-    if not serve_static or not DEBUG:
+    if not serve_static:
         STATIC_URL = 'https://fashionistavanced.s3.eu-north-1.amazonaws.com/'
         ALLOWED_HOSTS = ['fashionistavanced.com', 'www.fashionistavanced.com']
     else:
-        STATIC_ROOT = '/tmp/statictemp'
+        STATIC_ROOT = '/home/ec2-user/DofusFashionistaVanced/fashionsite/staticfiles'
         STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 LOGGING = {
