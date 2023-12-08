01d66797e90023135617fa11a3aef91fbe119344
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-08 14:03:28 +0100

    fix order by hits

--- fashionsite/chardata/item_exchange.py
+++ fashionsite/chardata/item_exchange.py
@@ -292,8 +292,7 @@ def _get_weapon_rate(weapon, char, result):
             total_damage -= (damage.min_dam + damage.max_dam)/2
         else:
             total_damage += (damage.min_dam + damage.max_dam)/2
-    print(weapon_obj.ap)
-    if float(weapon_obj.ap) < 1:
+    if weapon_obj.ap == 0:
         rating_non_crit = 0
     else:
         rating_non_crit = total_damage / float(weapon_obj.ap)
@@ -311,7 +310,10 @@ def _get_weapon_rate(weapon, char, result):
                 total_damage -= (damage.min_dam + damage.max_dam)/2
             else:
                 total_damage += (damage.min_dam + damage.max_dam)/2
-        rating_crit = total_damage / float(weapon_obj.ap)
+        if weapon_obj.ap == 0:
+            rating_crit = 0
+        else:
+            rating_crit = total_damage / float(weapon_obj.ap)
     
     crits_total = new_stats['ch']
     if weapon_obj.crit_chance_percent:
