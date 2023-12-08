2350a76cf31b07161853f6b166567d56e51d07a7
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-08 17:41:12 +0100

    Update run_fashionista_production.sh

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -4,4 +4,5 @@ bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
 cd ..
-gunicorn fashionsite.fashionsite.wsgi:application --bind 0.0.0.0:8000
+export DJANGO_SETTINGS_MODULE=fashionsite.settings
+gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
