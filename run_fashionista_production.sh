563f48e76d50e68d91e375fd8da508f38e1d3a7d
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-08 17:42:45 +0100

    Update run_fashionista_production.sh

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -1,8 +1,8 @@
-export PYTHONPATH=$PYTHONPATH:~/DofusFashionistaVanced
+export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
 cd ~/DofusFashionistaVanced
 bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
 cd ..
 export DJANGO_SETTINGS_MODULE=fashionsite.settings
-gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
+gunicorn fashionsite.fashionsite.wsgi:application --bind 0.0.0.0:8000
