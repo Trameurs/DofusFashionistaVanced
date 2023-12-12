5a2a98c4670eb2052ddab87cb48776dd7c18c72d
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-12 14:28:11 +0100

    added google auth

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -1,5 +1,7 @@
 cd ~/DofusFashionistaVanced
 export PYTHONPATH=$PYTHONPATH:$(pwd)
+export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced/fashionsite
+export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
 bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
