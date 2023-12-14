92b12a5fe26edf083a2e6dc1a231cfdd0ebf245a
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-14 12:44:53 +0100

    added some spells + images for Forgelance

--- fashionistapulp/fashionistapulp/dofus_constants.py
+++ fashionistapulp/fashionistapulp/dofus_constants.py
@@ -3228,6 +3228,109 @@ DAMAGE_SPELLS = {
             [['30-34', '34-38']],
             [FIRE],
         ), is_linked=(2, 'Biting Trident')),
+        Spell('Burning Estoc', [30, 97, 164], Effects(
+            [['14-16', '19-21', '23-26']],
+            [['17-20', '22-25', '28-31']],
+            [FIRE],
+        ), is_linked=(1, 'Octave')),
+        Spell('Octave', [140], Effects(
+            [['16-18']],
+            [['19-22']],
+            [WATER],
+        ), is_linked=(2, 'Burning Estoc')),
+        Spell('Brass Volley', [35, 102, 169], Effects(
+            [['14-16', '18-20', '22-25']],
+            [['17-19', '21-24', '26-30']],
+            [AIR],
+        ), is_linked=(1, 'Upheaval')),
+        Spell('Upheaval', [145], Effects(
+            [['29-33']],
+            [['35-40']],
+            [EARTH],
+        ), is_linked=(2, 'Brass Volley')),
+        Spell('Balestra', [40, 107, 174], Effects(
+            [['18-20', '23-25', '28-31']],
+            [['21-23', '27-30', '34-37']],
+            [WATER],
+        ), is_linked=(1, 'Windmill')),
+        Spell('Windmill', [150], Effects(
+            [['29-33']],
+            [['35-40']],
+            [AIR],
+        ), is_linked=(2, 'Balestra')),
+        Spell('Earthen Weakness', [45, 112, 179], Effects(
+            [['8-10', '11-13', '14-16']],
+            [['10-12', '13-15', '17-19']],
+            [EARTH],
+        ), is_linked=(1, 'Lunge')),
+        Spell('Lunge', [155], Effects(
+            [['12-14']],
+            [['14-17']],
+            [FIRE],
+        ), is_linked=(2, 'Earthen Weakness')),
+        Spell('Kyrja', [50, 117, 184], Effects(
+            [['18-20', '24-27', '28-32']],
+            [['21-24', '29-33', '34-38']],
+            [EARTH, FIRE, WATER, AIR],
+        ), aggregates=[('Steal in best element', [0]),
+                        ('', [1]),
+                        ('', [2]),
+                        ('', [3])],
+        steals=[True, True, True, True], 
+        is_linked=(1, 'Vajra')),
+        Spell('Vajra', [160], Effects(
+            [['39-44']],
+            [['47-53']],
+            [EARTH, FIRE, WATER, AIR],
+        ), aggregates=[('Steal in best element', [0]),
+                        ('', [1]),
+                        ('', [2]),
+                        ('', [3])],
+        steals=[True, True, True, True],
+        is_linked=(2, 'Kyrja')),
+        Spell('Maelstrom', [55, 122, 189], Effects(
+            [['11-13', '16-18', '18-20']],
+            [['14-16', '19-21', '22-24']],
+            [FIRE],
+        ), is_linked=(1, 'Ydra')),
+        Spell('Ydra', [165], Effects(
+            [['25-28']],
+            [['30-34']],
+            [EARTH],
+        ), is_linked=(2, 'Maelstrom')),
+        Spell('Iron Prelude', [60, 127, 194], Effects(
+            [['100'],
+             ['150'],
+             ['200']],
+            None,
+            ['buff_pow','buff_pow','buff_pow']
+        ), aggregates=[('Option 1', [0]),
+                       ('Option 2', [1]),
+                       ('Option 3', [2])]), 
+        Spell('Cyclone Lancer', [65, 131, 198], Effects(
+            [['23-25', '28-30', '31-33']],
+            [['28-30', '33-35', '37-40']],
+            [AIR],
+        ), is_linked=(1, 'Elding')),
+        Spell('Elding', [175], Effects(
+            [['32-36']],
+            [['38-43']],
+            [WATER],
+        ), is_linked=(2, 'Cyclone Lancer')),
+        Spell('Jormun', [75, 142], Effects(
+            [['24-27', '30-34']],
+            [['29-33', '36-41']],
+            [['21-24', '26-30']],
+            [['25-29', '31-36']],
+            [WATER],
+        ), aggregates=[('On ennemy', [0, 1]),
+                        ('On caster', [2, 3])],
+        is_linked=(1, 'Muspel')),
+        Spell('Muspel', [185], Effects(
+            [['32-36']],
+            [['38-42']],
+            [FIRE],
+        ), is_linked=(2, 'Jormun')),
     ]
 }
 
