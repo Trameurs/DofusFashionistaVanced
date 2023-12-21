89466983673c5d029bc087c649011923eb9edf3f
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-21 12:07:50 +0100

    Update dofus_constants.py

--- fashionistapulp/fashionistapulp/dofus_constants.py
+++ fashionistapulp/fashionistapulp/dofus_constants.py
@@ -2747,8 +2747,8 @@ DAMAGE_SPELLS = {
                        ('Allies/Summons', [1])],
         is_linked=(1, 'Force of Nature')),
         Spell('Force of Nature', [160], Effects(
-            create_stacking_values((('21-27'),), 15, 6),
-            create_stacking_values((('29-36'),), 15, 6),
+            create_stacking_values(((21, 27),), 15, 6),
+            create_stacking_values(((29, 36),), 15, 6),
             [EARTH] * 6,
         ), aggregates=[(TREE_LABELS[n], [n]) for n in range(6)],
         is_linked=(2, 'Dolly Sacrifice')),
