910d7dba7ca62e6acc09ac8f19c819b02978d2c4
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-23 14:51:52 +0100

    collectstatic too slow

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -5,6 +5,5 @@ export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
 bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
-python manage.py collectstatic --noinput
 cd ..
 gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
