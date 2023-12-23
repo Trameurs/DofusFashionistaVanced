63089491f751f6669232d747038de1f92d70c743
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-23 14:33:14 +0100

    add collectstatic to script

--- run_fashionista.sh
+++ run_fashionista.sh
@@ -1,5 +1,6 @@
 PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
+python manage.py collectstatic --noinput
 cd ..
 PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp bash -c 'python fashionsite/manage.py runserver 0.0.0.0:8000'
--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -5,5 +5,6 @@ export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
 bash -c './wipe_solution_cache.py'
 cd fashionsite
 bash -c 'django-admin compilemessages'
+python manage.py collectstatic --noinput
 cd ..
 gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
