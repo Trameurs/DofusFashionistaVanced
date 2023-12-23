318d8c0e7691d041480c2e650f1299c1b7c31ed4
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-23 19:59:37 +0100

    Update fashion_action.py

--- fashionsite/chardata/fashion_action.py
+++ fashionsite/chardata/fashion_action.py
@@ -34,7 +34,7 @@ from fashionistapulp.model import ModelInput
 from fashionistapulp.model_pool import create_model, borrow_model, return_model
 
 
-if not settings.DEBUG:
+if settings.DEBUG:
     create_model()
 
 MEMORY = DatabaseSolutionMemory()
