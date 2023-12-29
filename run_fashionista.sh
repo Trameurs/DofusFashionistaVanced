a4fb90ac1550364b1defd51ce0aebb9024de04e0
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-29 10:44:38 +0100

    Update run_fashionista.sh

--- run_fashionista.sh
+++ run_fashionista.sh
@@ -1,5 +1,15 @@
-PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp bash -c './wipe_solution_cache.py'
-cd fashionsite
-bash -c 'django-admin compilemessages'
-cd ..
-PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp bash -c 'python fashionsite/manage.py runserver 0.0.0.0:8000'
+#!/bin/bash
+
+while true
+do
+    export PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp
+    bash -c './wipe_solution_cache.py'
+    cd fashionsite
+    bash -c 'django-admin compilemessages'
+    cd ..
+    export PYTHONPATH=$PYTHONPATH:~/fashionista/fashionistapulp
+    bash -c 'python fashionsite/manage.py runserver 0.0.0.0:8000'
+
+    echo "Server crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
