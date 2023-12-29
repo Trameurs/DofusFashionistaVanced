cb5db164123fc4c02f5603e2b3fb34e3b9a2f2ae
Trameur <82444971+Trameurs@users.noreply.github.com>
2023-12-29 10:46:04 +0100

    Update run_fashionista_production.sh

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -1,9 +1,17 @@
-cd ~/DofusFashionistaVanced
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced/fashionsite
-export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
-bash -c './wipe_solution_cache.py'
-cd fashionsite
-bash -c 'django-admin compilemessages'
-cd ..
-gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
+#!/bin/bash
+
+while true
+do
+    cd ~/DofusFashionistaVanced
+    export PYTHONPATH=$PYTHONPATH:$(pwd)
+    export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced/fashionsite
+    export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
+    bash -c './wipe_solution_cache.py'
+    cd fashionsite
+    bash -c 'django-admin compilemessages'
+    cd ..
+    gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
+
+    echo "Server crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
