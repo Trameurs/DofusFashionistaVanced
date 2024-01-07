dae31fb542ad5de3f8d9454a7be0e4d9b1b5401b
Trameur <82444971+Trameurs@users.noreply.github.com>
2024-01-07 15:37:39 +0100

    fixed some items with -AP or -MP from target effect

--- itemscraper/get_equipments2.py
+++ itemscraper/get_equipments2.py
@@ -160,6 +160,7 @@ for item in equipment_data['en']['items']:
                 eff["int_maximum"] if not eff["ignore_int_max"] else None,
                 eff["type"]["name"]
             ] for eff in item["effects"]
+            if not ((eff["type"]["name"] == 'MP' and item["name"] in ["War's Halbaxe", "Wulan's Bow", "Roasty Breadstick", "Pillar of Ephedrya", "Imp Sword", "Phonemenal Scythe"]) or (eff["type"]["id"] == 179))
         ]
         for eff in item["effects"]:
             if eff["type"]["name"] == '-special spell-':
