49b00efcdd81f4c4c1573bb3f65ca38f51d34e22
Trameur <thibaud.mourey@gmail.com>
2024-12-29 19:41:44 +0100

    extended timeout

--- run_fashionista_production.sh
+++ run_fashionista_production.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Trap Ctrl+C
+trap 'echo "Stopping server..."; exit' INT
+
+BASE_DIR="$HOME/DofusFashionistaVanced"
+FASHIONSITE_DIR="$BASE_DIR/fashionsite"
+
+export PYTHONPATH="$PYTHONPATH:$BASE_DIR:$FASHIONSITE_DIR"
+
 while true
 do
-    cd ~/DofusFashionistaVanced
-    export PYTHONPATH=$PYTHONPATH:$(pwd)
-    export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced/fashionsite
-    export PYTHONPATH=$PYTHONPATH:/home/ec2-user/DofusFashionistaVanced
-    bash -c './wipe_solution_cache.py'
-    cd fashionsite
-    bash -c 'django-admin compilemessages'
-    cd ..
-    gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000
+    cd "$BASE_DIR" || exit 1
 
-    echo "Server crashed with exit code $?.  Respawning.." >&2
+    bash -c './wipe_solution_cache.py'
+
+    cd "$FASHIONSITE_DIR" || exit 1
+    bash -c 'django-admin compilemessages'
+
+    cd "$BASE_DIR" || exit 1
+
+    bash -c 'gunicorn fashionsite.wsgi:application --bind 0.0.0.0:8000 --timeout 150'
+
+    echo "Server crashed with exit code $?. Respawning..." >&2
     sleep 1
 done
